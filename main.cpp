0108ee29b1f0b42936136371254757ecb1cb07a3
Krzysztof Urbanowski <krzysztof.urbanowski@mobica.com>
2018-09-09 09:55:49 +0100

    Main metod sets time based random seed.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "renderarea.h"
 #include "slider_mainwindow.h"
 #include <QApplication>
+#include <QRect>
+#include "grid.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     SliderMainWindow w;
-
+    srand (time(0));
     w.show();
 
     return a.exec();
